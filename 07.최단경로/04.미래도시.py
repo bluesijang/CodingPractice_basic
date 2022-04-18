763feeb51751843ebe10f6a145b860ffaa99cb52
sijang <bluesijang@gmail.com>
2022-04-18 21:19:43 +0900

    최단경로 수정

--- 07.최단경로/04.미래도시.py
+++ 07.최단경로/04.미래도시.py
@@ -4,8 +4,9 @@
 # X 회사를 가기 전 K 회사를 방문 후 X 회사에 도착
 
 # 1st : 1 <= 회사수 n, 경로 수 m <= 100
-# 2nd : m 정보 (X, K)
-# 방문원 A가 K를 거처 X로 가는 최소 이동시간을 구하시고
+# 2nd : m 정보
+# 마지막줄 : 거쳐갈 x와 최종 목적지 K (X, K)
+# 방문원 A가 K를 거처 X로 가는 최소 이동시간을 구하시오
 
 # copy & paste
 input_data1 = """
@@ -25,3 +26,33 @@ input_data2 = """
 2 4
 3 4
 """
+n, m = map(int, input().split())
+INF = int(1e9)
+
+graph = [[INF] * (n+1) for _ in range(n+1)]
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if i == j:
+            graph[i][j] = 0
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+x, k = map(int, input().split())
+
+for t in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][t]+graph[t][b])
+
+distance = graph[1][k] + graph[k][x]
+
+for i in graph:
+    print(i)
+
+if distance >= INF:
+    print(-1)
+else:
+    print(distance)
--- 07.최단경로/05.전보.py
+++ 07.최단경로/05.전보.py
@@ -2,8 +2,59 @@
 # X -> Y 도시로 연결(방향성 있음) 정보가 있다
 # C도시에서 위급사항이 발생했을 때 보낸 메세지를 받게되는 최대 도시 수를 구하시오
 
-# 1st : 도시 수 n, 통로의 수 m, 메세지를 보내는 도시 C
+# 1st : 도시의 개수n, 통로의 개수m, 메세지를 보내고자 하는 도시 C
 #       1<=n<=30,000    1<=m<=200,000   1<=c<=n
 # 2nd ~ m+1줄 : 통로에 대한 정보 x, y, z
-#               x도시에서 Y도시 통로로 전달시간은 Z가 소요됨
+#               x → Y도시로 전달시간은 z
 # output : C가 보낸 메세지를 받는 도시의 총 개수와 총 걸리는 시간을 공백으로 구분하여 출력하시오
+
+import heapq
+
+input_data1 = """
+3 2 1
+1 2 4
+1 3 2
+"""
+n, m, start = map(int, input().split())
+INF = int(1e9)
+distance = [INF] * (n+1)
+graph = [[] for _ in range(n+1)]
+
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))   # heapq (distance, node)
+    distance[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+        print(f"dist, now :{dist}, {now}")
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+
+dijkstra(start)
+
+print(graph)
+print(distance)
+
+count = 0
+max_value = 0
+
+for i in distance:
+    if i != INF:
+        count += 1
+        if max_value < i:
+            max_value = i
+
+print(count-1, max_value)
